1e8ea7e4f5868e695afe14c6ffc1c9f4325fbd8c
Plamen Petrov <biznetmen@gmail.com>
2020-11-22 17:17:33 +0200

    Added 2 more recipes

--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'Test description', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700%2C636')
+    new Recipe('Test Recipe', 'Test description', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'),
+    new Recipe('Second Recipe', 'Second description', 'https://thedefineddish.com/wp-content/uploads/2019/12/2019-12-28-22.37.12.jpg'),
+    new Recipe('Third Recipe', 'Third description', 'https://pbs.twimg.com/profile_images/1093186805283749890/4yb0033d.jpg')
   ];
   
   constructor() { }

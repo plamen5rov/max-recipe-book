130b60b00f4f94aadbc40f0eda690598dade8229
Plamen Petrov <biznetmen@gmail.com>
2020-11-24 15:00:47 +0200

    Allowing the User to Add Ingredients to the Shopping List

--- recipe-app/src/app/shopping-list/shopping-list.component.ts
+++ recipe-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { Component, OnInit } from '@angular/core';
-import { Ingredient } from '../shared/ingredient.model';
+
 
 @Component({
   selector: 'app-shopping-list',
@@ -17,4 +18,7 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }

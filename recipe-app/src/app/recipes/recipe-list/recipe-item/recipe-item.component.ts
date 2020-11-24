fed73484fbd756d352ac4165b1c4801e31c9edfd
Plamen Petrov <biznetmen@gmail.com>
2020-11-24 08:55:16 +0200

    Passing Recipe Data with Property Binding

--- recipe-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { Recipe } from './../../recipe.model';
+import { Component, Input, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
-export class RecipeItemComponent implements OnInit {
+
+
+export class RecipeItemComponent implements OnInit  {
+ @Input() recipe: Recipe;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+   }
 
 }
